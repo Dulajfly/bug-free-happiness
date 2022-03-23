0a19d0f594396e9812a8dc7da139ceaa202c5c78
Dulajfly <jakubdulaj@outlook.com>
2022-03-23 22:35:49 +0100

    Add main and Login view with html

--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class Login(LoginView):
+    template_name = 'user/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('main')
+
+class MainView(TemplateView):
+    template_name = 'main.html'
